16883dfe30a5758b988c100b65fc1ed36e147608
Saul Soto <saulivan852@gmail.com>
2021-04-27 21:40:19 -0700

    Moved components back to App for simplicity

--- src/App.vue
+++ src/App.vue
@@ -13,20 +13,79 @@
             -->
       <!--
     <v-app-bar app>
-      <v-banner>Unit Test Chellenge</v-banner>
+      <v-banner>Unit Test Challenge</v-banner>
     </v-app-bar>
     -->
 
       <!-- Sizes your content based upon application components -->
       <v-main>
-        <div class="left-column">
-          <Form />
-        </div>
-        <div class="right-column"><DataIterator /></div>
-        <v-container fluid>
-          <!-- If using vue-router -->
-          <router-view></router-view>
-        </v-container>
+        <v-row>
+          <v-col>
+            <v-container>
+              <v-form v-model="valid">
+                <v-container>
+                  <v-row>
+                    <v-col cols="12" md="4">
+                      <v-text-field
+                        v-model="clientes.Nombre"
+                        label="Nombre"
+                        required
+                      ></v-text-field>
+                      <v-text-field
+                        v-model="clientes.ApellidoPaterno"
+                        label="Apellido Paterno"
+                        required
+                      ></v-text-field>
+                      <v-text-field
+                        v-model="clientes.ApellidoMaterno"
+                        label="Apellido Materno"
+                        required
+                      ></v-text-field>
+                      <v-text-field
+                        v-model="Edad"
+                        label="Edad"
+                        required
+                      ></v-text-field>
+                      <v-text-field
+                        v-model="clientes.Ocupacion"
+                        label="Ocupacion"
+                        required
+                      ></v-text-field>
+                      <v-text-field
+                        v-model="clientes.SalarioMensual"
+                        label="Salario Mensual"
+                        required
+                      ></v-text-field>
+                      <v-text-field
+                        v-model="clientes.ModeloVehiculo"
+                        :counter="4"
+                        label="Modelo Vehiculo"
+                        required
+                      ></v-text-field>
+                      <v-container>
+                        <v-btn color="primary" v-on:click="handleSave"
+                          >Guardar</v-btn
+                        >
+                        <v-btn color="primary">Nuevo</v-btn>
+                        <v-btn color="primary">Eliminar</v-btn>
+                      </v-container>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-form>
+            </v-container>
+          </v-col>
+          <v-col>
+            <v-container>
+              <v-data-table
+                :headers="headers"
+                :items="clientes"
+                :items-per-page="5"
+                class="elevation-1"
+              ></v-data-table>
+            </v-container>
+          </v-col>
+        </v-row>
       </v-main>
 
       <v-footer app>
@@ -37,18 +96,47 @@
 </template>
 
 <script>
-import Form from "../src/components/Form";
-import DataIterator from "../src/components/List";
 export default {
   name: "App",
-
-  components: {
-    Form,
-    DataIterator,
+  methods: {
+    handleSave() {
+      var index = 0;
+      console.log(this.clientes[index].Nombre);
+      index = index + 1;
+      this.clientes[index] = this.clientes.Nombre;
+      console.log(this.clientes[index].Nombre);
+    },
   },
-
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      headers: [
+        {
+          text: "Nombre",
+          align: "start",
+          sortable: false,
+          value: "Nombre",
+        },
+        { text: "Apellido Paterno", value: "ApellidoPaterno" },
+        { text: "Apellido Materno", value: "ApellidoMaterno" },
+        { text: "Edad", value: "Edad" },
+        { text: "Ocupacion", value: "Ocupacion" },
+        { text: "Salario Mensual", value: "SalarioMensual" },
+        { text: "Modelo Vehiculo", value: "ModeloVehiculo" },
+        { text: "Seguro disponible", value: "SeguroDisponible" },
+      ],
+      clientes: [
+        {
+          Nombre: "Saul Ivan",
+          ApellidoPaterno: "Soto",
+          ApellidoMaterno: "Pino",
+          Edad: 24,
+          Ocupacion: "Estudiante / Trabajo",
+          SalarioMensual: "12000",
+          ModeloVehiculo: 2012,
+          SeguroDisponible: "NA",
+        },
+      ],
+    };
+  },
 };
 </script>
